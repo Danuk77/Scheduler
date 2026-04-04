1016f88c599999902c6bbca3cf7efbce2a616880
danuk <danukdons@gmail.com>
2026-04-04 21:13:01 +0530

    Refactored hill_climber.rs to be more readable

--- src/hill_climber.rs
+++ src/hill_climber.rs
@@ -29,42 +29,65 @@ pub fn run_hill_climber(constraints: &mut ConstraintStore, iterations: u32) -> (
 
     for iteration in 0..iterations {
         debug!("Running iteration number {:?}", iteration);
-
-        if let Some(changes) = evolve_schedule(constraints, &penalties, &mut schedule) {
-            let (new_penalties, new_total_penalty) = calculate_penalties(constraints, &schedule);
-            debug!("Evaluated penalty. Penalty: {:?}", new_total_penalty);
-
-            if new_total_penalty <= total_penalty {
-                total_penalty = new_total_penalty;
-                penalties = new_penalties;
-
-                if total_penalty <= best_total_penalty {
-                    best_total_penalty = total_penalty;
-                    best_schedule = schedule.clone();
-                }
-            } else {
-                debug!("New penalty is worse than existing best penalty");
-                let delta_penalty: f32 = new_total_penalty as f32 - total_penalty as f32;
-                if (-delta_penalty / temperature).exp() > random() {
-                    debug!("Accepting worse schedule");
-                    total_penalty = new_total_penalty;
-                    penalties = new_penalties;
-                } else {
-                    debug!("Reverting");
-                    changes
-                        .iter()
-                        .rev()
-                        .for_each(|change| change.revert_change(&mut schedule));
-                }
-            }
-            temperature *= cooling_factor;
-        } else {
+        let Some(changes) = evolve_schedule(constraints, &penalties, &mut schedule) else {
             debug!(
                 "Did not find optimisation schedule at iteration {:?}",
                 iteration
             );
+            temperature *= cooling_factor;
+            continue;
+        };
+
+        let (new_penalties, new_total_penalty) = calculate_penalties(constraints, &schedule);
+        debug!("Evaluated penalty. Penalty: {:?}", new_total_penalty);
+
+        if let false = _should_evoltion_be_accepted(new_total_penalty, total_penalty, temperature) {
+            debug!("Evolution not accepted. Reverting");
+            changes
+                .iter()
+                .rev()
+                .for_each(|change| change.revert_change(&mut schedule));
+        };
+
+        total_penalty = new_total_penalty;
+        penalties = new_penalties;
+
+        if total_penalty <= best_total_penalty {
+            best_total_penalty = total_penalty;
+            best_schedule = schedule.clone();
         }
+
+        temperature *= cooling_factor;
     }
 
     (best_schedule, best_total_penalty)
+}
+
+/// Evaluated whether an evolution in the schedule should be accepted or not based on the realised
+/// penalties
+///
+/// # Arguments
+/// * `new_total_penalty` - The penalties realised for the new schedule
+/// * `existing_total_penalty` - The penalties realised for the schedule before the evolution
+/// * `temperature` - The current temperature in the annealing algorithm
+///
+/// # Returns
+/// * `true` - If the evolution should be accepted
+/// * `false` - Otherwise
+fn _should_evoltion_be_accepted(
+    new_total_penalty: u32,
+    existing_total_penalty: u32,
+    temperature: f32,
+) -> bool {
+    let delta_penalty: f32 = new_total_penalty as f32 - existing_total_penalty as f32;
+
+    if delta_penalty <= 0.0 {
+        return true;
+    }
+
+    if (-delta_penalty / temperature).exp() > random() {
+        return true;
+    }
+
+    false
 }
