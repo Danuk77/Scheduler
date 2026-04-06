a669576b6ce6719f52ce18ddbdff54b3d7b137b9
danuk <danukdons@gmail.com>
2026-04-06 20:29:39 +0530

    Allowed dead code for constraint builder

--- src/constraints/constraint_builder.rs
+++ src/constraints/constraint_builder.rs
@@ -5,6 +5,7 @@ use crate::{
     schedule::Slot,
 };
 
+#[allow(dead_code)]
 /// A simple builder class used for building constraints
 /// As part of building the constraint, its relevant penalty function is generated.
 /// The generated penalty function depends on the parameters used when building the constraint.
@@ -20,6 +21,7 @@ pub struct ConstraintBuilder {
     frequency: Option<u8>,
 }
 
+#[allow(dead_code)]
 impl ConstraintBuilder {
     pub fn new() -> ConstraintBuilder {
         ConstraintBuilder {
